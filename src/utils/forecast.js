e23474c3a67f41896d633416dcf62fd8a6f3842a
Christian Kobril <christian.kobril@studenti.fauser.edu>
2019-03-21 23:38:51 +0100

    Change to celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'https://api.darksky.net/forecast/a2d13e9a54f055a96d891539cc9da18f/' + latitude + ',' + longitude
+  const url = 'https://api.darksky.net/forecast/a2d13e9a54f055a96d891539cc9da18f/' + latitude + ',' + longitude + '?units=si'
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -9,8 +9,8 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out but it feels like ' + body.currently.apparentTemperature +
-      '. High today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '° out but it feels like ' + body.currently.apparentTemperature +
+      '°. High today is ' + body.daily.data[0].temperatureHigh + '° with a low of ' + body.daily.data[0].temperatureLow + '°. There is a ' + body.currently.precipProbability + '% chance of rain.')
     }
   })
 }
